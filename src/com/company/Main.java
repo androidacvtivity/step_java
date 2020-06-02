ca570cf6e162241d492c67ab21f270430ddf8412
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-06-02 16:53:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public enum Season { WINTER, SPRING, SUMMER, FALL }
+
+    public static void main(String[] args) {
+	// write your code here
+        for (Season s : Season.values())
+            System.out.println(s);
+    }
+}
