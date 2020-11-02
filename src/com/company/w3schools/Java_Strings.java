c0fcfb431ab1633e4e2428d7c4d0ed716b79e6f1
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-11-02 13:09:19 +0200

    String Length

---
+++ src/com/company/w3schools/Java_Strings.java
@@ -0,0 +1,17 @@
+package com.company.w3schools;
+
+public class Java_Strings {
+
+    public static void main (String[] args){
+        String greeting = "Hello";
+        System.out.println(greeting);
+        System.out.println(greeting + " length "+ greeting.length());
+        System.out.println(greeting.length());
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        System.out.println(txt);
+        System.out.println("The length of the txt string is: " + txt.length());
+
+
+    }
+}
