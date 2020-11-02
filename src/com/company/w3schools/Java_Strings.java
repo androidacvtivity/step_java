5c6d322411e942346ad0f0cc586496b6b91fea4c
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-11-02 15:01:26 +0200

    String

--- src/com/company/w3schools/Java_Strings.java
+++ src/com/company/w3schools/Java_Strings.java
@@ -13,5 +13,35 @@ public class Java_Strings {
         System.out.println("The length of the txt string is: " + txt.length());
 
 
+        String text = "Hello World";
+        System.out.println(text.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(text.toLowerCase());   // Outputs "hello world"
+
+        String txt3 = "Please locate where 'locate' occurs!";
+        System.out.println(txt3.indexOf("locate")); // Outputs 7
+
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + " " + lastName);
+
+
+
+        String firstName1 = "Vitalie ";
+        String lastName1 = "Bancu";
+        System.out.println(firstName1.concat(lastName1));
+
+
+        String txt4 = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt4);
+
+        String txt5 = "It\'s alright.";
+        System.out.println(txt5);
+
+        String txt6 = "The character \\ is called backslash.";
+        System.out.println(txt6);
+
+
+
+
     }
 }
