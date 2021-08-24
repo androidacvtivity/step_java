024d63a866a2e54e89c30ef5222be2b43544d526
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-08-24 18:15:04 +0300

    Test - https://www.caveofprogramming.com/java/java-static-class-tutorial.html

---
+++ src/com/company/w3schools/CarParts.java
@@ -0,0 +1,14 @@
+package com.company.w3schools;
+
+public class CarParts {
+
+    public static class Wheel {
+        public Wheel() {
+            System.out.println("Wheel created!");
+        }
+
+        public void CarParts() {
+            System.out.println("Car Parts object created!");
+        }
+    }
+}
